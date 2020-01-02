7fe945756d6a2558d85ee92050a29182de1e2538
Wei Ruan (Patrick) <acdmc.wruan@gmail.com>
2020-01-02 17:05:48 -0500

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,30 @@
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
+import tensorflow as tf
+
+
+IMAGE_WIDTH=124
+IMAGE_HEIGHT=124
+
+if __name__== "__main__":
+
+    path = input("Where is your image path?")
+    image = tf.io.read_file(path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [IMAGE_WIDTH, IMAGE_HEIGHT])
+    image /= 255.0
+
+    model=tf.keras.models.Model()
+    model.load('.vggmodel/saved_model.pb')
+    labels={0:'cat',1:'dog'}
+    y=model.predict_generator(image)
+    index=0
+    if y >0.5:
+        index=1
+    else:
+        index=0
+    print("This image is a"+labels[index])
